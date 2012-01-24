bbb54553dc975fcf4938fad73514637785d1f015
Juan Francisco Navarrete Martinez <jfco.navarrete@gmail.com>
2012-01-24 16:58:17 -0600

    agregue un ciclo for

---
+++ src/main/java/mx/jfnm/ejemplo/gittest/MiClase.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.jfnm.ejemplo.gittest;
+
+/**
+ *
+ * @author Juan Fco
+ */
+public class MiClase {
+    
+}
