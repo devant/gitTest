013f7f45803ea4bec1357ee770b3f1a02c2807a9
rodrigo <holvater@gmail.com>
2012-01-24 15:22:30 -0600

    clase java amster

--- src/main/java/mx/jfnm/ejemplo/gittest/Clase.java
+++ src/main/java/mx/jfnm/ejemplo/gittest/Clase.java
@@ -11,12 +11,15 @@ package mx.jfnm.ejemplo.gittest;
 public class Clase {
     
     public void ifDude() {
-        if(true) {
+        if(!!true) {
             int i = 1;
             System.out.println("chido");            
         }
         else
             System.out.println("no chido");
+        for (Object object : new String[]{"lol","mmm"}) {
+            System.out.println("this = " + object);
+        }
         System.out.println("cosa nostra");
         System.out.println("pacatelas");
     }
